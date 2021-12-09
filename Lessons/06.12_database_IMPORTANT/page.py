5b2ab5d00051b9765688399e87d4915cc88f2de0
mrslv-bss <bassmiroslav@gmail.com>
2021-12-09 19:48:54 +0200

    Update page.py

--- Lessons/06.12_database_IMPORTANT/page.py
+++ Lessons/06.12_database_IMPORTANT/page.py
@@ -2,3 +2,4 @@ from selenium.webdriver.common.by import By
 
 search_field_loc = (By.ID, "twotabsearchtextbox")
 search_button_loc = (By.ID, "nav-search-submit-button")
+cell_loc = (By.XPATH, "//div[@data-component-type='s-search-result']")
