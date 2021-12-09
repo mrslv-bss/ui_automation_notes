56530148d0544a21f462a517b4f66c653edb95fc
mrslv-bss <bassmiroslav@gmail.com>
2021-12-09 20:36:19 +0200

    Added dbb commit

--- Lessons/06.12_database_IMPORTANT/test_find_and_compare.py
+++ Lessons/06.12_database_IMPORTANT/test_find_and_compare.py
@@ -12,11 +12,21 @@ def test_find_and_compare(get_driver):
     get_driver.find_element(*search_field_loc).send_keys(product)
     get_driver.find_element(*search_button_loc).click()
 
+    cursor = conn.cursor()
+    cursor.execute("""
+                    IF OBJECT_ID('dbo.MYROSLAV_TABLE', 'U') IS NOT NULL
+                        DROP TABLE dbo.MYROSLAV_TABLE
+                    CREATE TABLE dbo.MYROSLAV_TABLE (
+                        price VARCHAR(100),
+                        title VARCHAR(1000)
+                    )
+                """)
+
     for b in range(10):
         element = WebDriverWait(get_driver, 10).until(expected_conditions.visibility_of_all_elements_located(cell_loc))
         element_number = element[b]
         data_asin_element = element_number.get_attribute("data-asin")
-        print(data_asin_element) # Product cell code
+        print(data_asin_element) # Product cell (identifier)
 
         price_int_loc = (By.XPATH, f"//div[@data-asin='{data_asin_element}'] //span[@class='a-price-whole']")
         price_dec_loc = (By.XPATH, f"//div[@data-asin='{data_asin_element}'] //span[@class='a-price-fraction']")
@@ -25,4 +35,19 @@ def test_find_and_compare(get_driver):
         if len(element_price_dec) == 0: # Skip, If price is absent
             continue
         price_loc = "$"+element_price_int[0].text+"."+element_price_dec[0].text # Connect integer and decimal parts
-        print(price_loc)
+        print(price_loc) # Total price format: $nn.nn
+
+        product_title_loc = (By.XPATH, f"//div[@data-asin='{data_asin_element}'] //span[@class='a-size-base-plus a-color-base a-text-normal']")
+        product_title_element = get_driver.find_element(*product_title_loc)
+        product_title = product_title_element.text
+        print(product_title) # Title
+
+        cursor.execute("INSERT INTO dbo.MYROSLAV_TABLE VALUES (%s, %s)", (price_loc, product_title))
+        conn.commit()
+
+    cursor.execute("select * from dbo.MYROSLAV_TABLE")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row) # Show result (pytest -s)
+
+    get_driver.close()
